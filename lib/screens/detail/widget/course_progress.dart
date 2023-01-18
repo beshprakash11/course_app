1162e36c2604ab75f040f5a14034d34b53e3a946
beshprakash <besh@arrtsm.com>
2023-01-18 14:25:30 +0100

    Progress completed

--- lib/screens/detail/widget/course_progress.dart
+++ lib/screens/detail/widget/course_progress.dart
@@ -1,9 +1,30 @@
+import 'package:course_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class CourseProgress extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'This progress',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 20, color: kFont
+            ),
+          ),
+          Row(
+            children: [
+              Image.asset('assets/icons/grid.png', width: 25,),
+              SizedBox(width: 15,),
+              Image.asset('assets/icons/list.png', width: 25,)
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
