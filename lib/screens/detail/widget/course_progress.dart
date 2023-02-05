856f2e9c25c917d08790e346c1534dd2423c3aec
beshprakash11 <beshprakashktm@gmail.com>
2023-02-05 13:17:56 +0100

    Course progress updated

--- lib/screens/detail/widget/course_progress.dart
+++ lib/screens/detail/widget/course_progress.dart
@@ -1,28 +1,34 @@
 import 'package:course_app/constants/colors.dart';
+import 'package:course_app/models/module.dart';
 import 'package:flutter/material.dart';
 
 class CourseProgress extends StatelessWidget {
+  final modulesList = Module.generateModules();
   
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Column(
         children: [
-          Text(
-            'This progress',
-            style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 20, color: kFont
-            ),
-          ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Image.asset('assets/icons/grid.png', width: 25,),
-              SizedBox(width: 15,),
-              Image.asset('assets/icons/list.png', width: 25,)
+              const Text(
+                'This progress',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 20, color: kFont
+                ),
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/icons/grid.png', width: 25,),
+                  SizedBox(width: 15,),
+                  Image.asset('assets/icons/list.png', width: 25,)
+                ],
+              )
             ],
-          )
+          ),
         ],
       ),
     );
