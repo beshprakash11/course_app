5bbabd91c8b0a37163268f110d92fdea45549888
beshprakash11 <beshprakashktm@gmail.com>
2023-02-05 13:32:59 +0100

    Flexible row added

--- lib/screens/detail/widget/course_module.dart
+++ lib/screens/detail/widget/course_module.dart
@@ -7,7 +7,11 @@ class CourseModule extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Text('Course Module'),
+      height: 180,
+      child: Row(children: [
+        Flexible(flex: 1,child: Container(color: Colors.red,)),
+        Flexible(flex: 5,child: Container(color: Colors.blue,)),
+      ],),
     );
   }
 }
