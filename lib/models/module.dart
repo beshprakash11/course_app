1561360df606eb5722fe3555f39ef363251ab95a
beshprakash11 <beshprakashktm@gmail.com>
2023-02-05 13:11:10 +0100

    Module updated

--- lib/models/module.dart
+++ lib/models/module.dart
@@ -4,4 +4,20 @@ class Module{
   Color iconBorder;
   Color iconBar;
   Color iconColor;
+  IconData icon;
+  String title;
+  String desc;
+  Color moudleBorder;
+  Color moduleBg;
+  Color buttonColor;
+  Color buttonFont;
+  String time;
+  String lession;
+  Module(
+    this.iconBorder, this.iconBar, this.iconColor,
+    this.icon, 
+    this.title, this.desc,
+    this.moudleBorder, this.moduleBg, this.buttonColor, this.buttonFont, 
+    this.time, this.lession
+  )
 }
